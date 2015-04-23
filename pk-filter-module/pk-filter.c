2c2c81447c630c6725c9dd397c8f4985a956e5b4
aakarsh <aakarsh.nair@gmail.com>
2015-04-22 20:44:58 -0700

    move around attribute stuff

--- pk-filter-module/pk-filter.c
+++ pk-filter-module/pk-filter.c
@@ -51,6 +51,35 @@ typedef enum pkfilter_cmd {
 } pkfilter_cmd_t;
 // end copy
 
+// Configuration Rules
+enum pk_rule_type {
+  PK_ACCEPT,
+  PK_DROP,
+  PK_LOG,
+  PK_LOG_HDR
+};
+
+enum pk_attr_type {
+  PK_AT_SRC  = 0,
+  PK_AT_DST = 1,
+  PK_AT_PROTO = 2,
+  PK_AT_MAX = 3
+};
+
+typedef struct pk_attr {
+  struct list_head list;
+  int type;
+  char* val;
+} pk_attr_t;
+
+typedef struct pk_cmd {
+  struct list_head list;
+  int type;
+  struct list_head attrs;
+} pk_cmd_t;
+
+
+
 #define PK_NL_CMD_MAX 2
 
 
@@ -144,32 +173,6 @@ static struct pernet_operations pk_filter_net_ops = {
 };
 
 
-// Configuration Rules
-enum pk_rule_type {
-  PK_ACCEPT,
-  PK_DROP,
-  PK_LOG,
-  PK_LOG_HDR
-};
-
-enum pk_attr_type {
-  PK_AT_SRC  = 0,
-  PK_AT_DST = 1,
-  PK_AT_PROTO = 2,
-  PK_AT_MAX = 3
-};
-
-typedef struct pk_attr {
-  struct list_head list;
-  int type;
-  char* val;
-} pk_attr_t;
-
-typedef struct pk_cmd {
-  struct list_head list;
-  int type;
-  struct list_head attrs;
-} pk_cmd_t;
 
 // List of packet filtering commands
 static LIST_HEAD(pk_cmds);
